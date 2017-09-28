28f76f0148a75802ac88d91ddfe7fa9fae47561f
Milos Vasic <milos85vasic@gmail.com>
2017-09-28 22:11:39 +0200

    Garage Lab + Bosch meetup: Nullability.

---
+++ Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meet_the_kotlin_lecture/2. 1. Nullability.kt
@@ -0,0 +1,11 @@
+package net.milosvasic.conferences.bosch.meet_the_kotlin_lecture
+
+/**
+ * Nothing happens since d4 is set to null.
+ * Most important, application does not crash!
+ */
+fun nullability() {
+
+    d4?.testMethod2()
+
+}
--- Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meet_the_kotlin_lecture/2. Nullability.kt
+++ Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meet_the_kotlin_lecture/2. Nullability.kt
@@ -1,2 +1,27 @@
 package net.milosvasic.conferences.bosch.meet_the_kotlin_lecture
 
+var d1 = 10
+
+/**
+ * Compiler complains!
+ */
+// var d2 : Int = null
+
+/**
+ * This is null, since there is no data type.
+ */
+var d3 = null
+
+/**
+ * d4 is explicitly set to null
+ */
+var d4 : TestClass2? = null
+
+class TestClass2 {
+
+    fun testMethod2(){
+        println("Kotlin")
+    }
+
+}
+
