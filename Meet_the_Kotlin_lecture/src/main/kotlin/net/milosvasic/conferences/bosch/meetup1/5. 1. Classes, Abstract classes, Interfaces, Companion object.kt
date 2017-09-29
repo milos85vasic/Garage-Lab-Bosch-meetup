e91fb1078e1bcadf33a81ac0874dd2acc8be426b
Milos Vasic <milos85vasic@gmail.com>
2017-09-29 11:05:31 +0200

    Garage Lab + Bosch meetup: Companion object.

--- Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meetup1/5. 1. Classes, Abstract classes, Interfaces, Companion object.kt
+++ Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meetup1/5. 1. Classes, Abstract classes, Interfaces, Companion object.kt
@@ -3,9 +3,9 @@ package net.milosvasic.conferences.bosch.meetup1
 /**
  * Abstract Classes
  */
-abstract class DummyAbstract1()
+abstract class DummyAbstract1
 
-abstract class DummyAbstract2() {
+abstract class DummyAbstract2 {
 
     abstract fun print()
 
--- Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meetup1/5. 3. Classes, Abstract classes, Interfaces, Companion object.kt
+++ Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meetup1/5. 3. Classes, Abstract classes, Interfaces, Companion object.kt
@@ -6,3 +6,68 @@ package net.milosvasic.conferences.bosch.meetup1
  * A companion object is a specific type of object declaration
  * that allows an object to act similar to static objects in other languages (such as Java).
  */
+class MyManagerClass {
+
+    companion object {
+        val URL = "http://exemple.com/"
+
+        fun getSomeMagic() = "Maaaagic ;)"
+    }
+
+}
+
+fun useCompanionObjectAndObject(){
+
+    val url = MyManagerClass.URL // No class instantiation.
+
+    val magic = MyManagerClass.getSomeMagic()
+
+    /**
+     * Using object with interface
+     */
+    val audio = object : Volume {
+        override fun volumeUp() {
+            println("volumeUp")
+        }
+
+        override fun volumeDown() {
+            println("volumeDown")
+        }
+
+        override fun setVolume(volume: Int) {
+            println("setVolume: $volume")
+        }
+
+        override fun getVolume(): Int {
+            return 0
+        }
+    }
+
+    audio.setVolume(10)
+
+    /**
+     * Using object with abstract class
+     */
+    val tv = object : Tv() {
+        override fun togglePower() {
+            // ...
+        }
+
+        override fun volumeUp() {
+            // ...
+        }
+
+        override fun volumeDown() {
+            // ...
+        }
+
+        override fun setVolume(volume: Int) {
+            // ...
+        }
+
+        override fun getVolume(): Int {
+            return 0
+        }
+    }
+
+}
