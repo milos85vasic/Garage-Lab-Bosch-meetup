750201f020c53d687d43ba103f82b63785babea9
Milos Vasic <milos85vasic@gmail.com>
2017-09-29 11:17:25 +0200

    Garage Lab + Bosch meetup: Data classes.

--- Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meetup1/6. Data classes.kt
+++ Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meetup1/6. Data classes.kt
@@ -1,2 +1,19 @@
 package net.milosvasic.conferences.bosch.meetup1
 
+/**
+ * Let's present some functions.
+ */
+data class UserApiResponse(
+        val userId: Int,
+        val address: String,
+        val metaData: Int = 0
+)
+
+// class ExtendedResponse : UserApiResponse()           <-- Compiler complains. Data class can't be inherited!
+
+fun main(args: Array<String>) {
+    val response = UserApiResponse(1, "Unknown")
+    println(response)                                   // Prints the object in human readable form.
+                                                        // output:
+                                                        // UserApiResponse(userId=1, address=Unknown, metaData=0)
+}
