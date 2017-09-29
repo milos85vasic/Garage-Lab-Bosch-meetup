c5d6754606ebb306691bcc268e59ded229c16f9d
Milos Vasic <milos85vasic@gmail.com>
2017-09-29 10:54:09 +0200

    Garage Lab + Bosch meetup: Companion object.

--- Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meetup1/5. 3. Classes, Abstract classes, Interfaces, Companion object.kt
+++ Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meetup1/5. 3. Classes, Abstract classes, Interfaces, Companion object.kt
@@ -1,2 +1,8 @@
 package net.milosvasic.conferences.bosch.meetup1
 
+/**
+ * Companion object
+ *
+ * A companion object is a specific type of object declaration
+ * that allows an object to act similar to static objects in other languages (such as Java).
+ */
