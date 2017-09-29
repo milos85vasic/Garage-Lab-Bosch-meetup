78a1646edea7fb447230dc503c5602d4169a29f4
Milos Vasic <milos85vasic@gmail.com>
2017-09-29 11:29:13 +0200

    Garage Lab + Bosch meetup: Extension functions.

--- Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meetup1/7. Extension functions.kt
+++ Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meetup1/7. Extension functions.kt
@@ -1,2 +1,10 @@
 package net.milosvasic.conferences.bosch.meetup1
 
+/**
+ * Extension functions
+ */
+fun String.strip(): String {
+    return this.replace("_", " ")
+}
+
+fun main(args: Array<String>) = println("STRIP_ME_NOW!".strip())
