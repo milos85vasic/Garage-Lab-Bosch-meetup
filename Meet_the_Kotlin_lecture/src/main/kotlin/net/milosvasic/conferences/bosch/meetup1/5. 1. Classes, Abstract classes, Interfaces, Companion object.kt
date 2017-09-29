63b61608e741fece84a2f31ac4a34d87c28529f9
Milos Vasic <milos85vasic@gmail.com>
2017-09-29 10:24:41 +0200

    Garage Lab + Bosch meetup: Classes.

--- Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meetup1/5. 1. Classes, Abstract classes, Interfaces, Companion object.kt
+++ Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meetup1/5. 1. Classes, Abstract classes, Interfaces, Companion object.kt
@@ -3,3 +3,10 @@ package net.milosvasic.conferences.bosch.meetup1
 /**
  * Abstract Classes
  */
+abstract class DummyAbstract1()
+
+abstract class DummyAbstract2(){
+
+    abstract fun print()
+
+}
--- Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meetup1/5. Classes, Abstract classes, Interfaces, Companion object.kt
+++ Meet_the_Kotlin_lecture/src/main/kotlin/net/milosvasic/conferences/bosch/meetup1/5. Classes, Abstract classes, Interfaces, Companion object.kt
@@ -55,3 +55,41 @@ class Example4(
 
     fun describeMe() = value.toString()
 }
+
+/**
+ * Inheritance
+ */
+class Example6
+
+// class Example7 : Example6()                  // Compiler complains since classes are by default closed!
+
+// So let's create one that we actually can extend!
+open class Example7 {
+
+    fun method1(){}
+
+    open fun method2(){}
+
+}
+
+class Example8 : Example7(){
+
+//    override fun method1() {                  <--- Compiler complains. Can't override method1() since it is not open!
+//        super.method2()
+//        println("And some more functionality added.")
+//    }
+
+    override fun method2() {
+        super.method2()
+        println("And some more functionality added.")
+    }
+}
+
+/**
+ * Class visibility
+ */
+class Example9                                  // Package level
+public class Example10                          // Publicly visible
+private class Example11                         // Visible only in the scope of the current context.
+internal class Example12                        // Visible only in the current module. This is great if making SDK!
+
